b2d989ca75afe8ee6a95026558331a2880a09679
Alex Rothuis <alex.rothuis@gmail.com>
2023-12-01 20:02:06 +0100

    Add coverage and improve PuzzleInput testing

--- src/main/java/nl/arothuis/aoc/core/PuzzleInput.java
+++ src/main/java/nl/arothuis/aoc/core/PuzzleInput.java
@@ -1,33 +1,19 @@
 package nl.arothuis.aoc.core;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class PuzzleInput {
-    public static String getFromFile(String year, String day) {
+public interface PuzzleInput {
+    static String getFromFile(String year, String day) {
         var filePath = "nl/arothuis/aoc/year" + year + "/day" + day + ".txt";
-        var inputStream = PuzzleInput.class
+        var resource = PuzzleInput.class
                 .getClassLoader()
-                .getResourceAsStream(filePath);
-
-        if (inputStream == null) {
-            throw new IllegalArgumentException(
-                    String.format("File path '%s' not found", filePath)
-            );
-        }
-
-        var result = new ByteArrayOutputStream();
+                .getResource(filePath);
 
         try {
-            byte[] buffer = new byte[1024];
-            for (int length; (length = inputStream.read(buffer)) != -1; ) {
-                result.write(buffer, 0, length);
-            }
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
+            return Files.readString(Paths.get(resource.toURI()));
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot read file", e);
         }
-
-        return result.toString(StandardCharsets.UTF_8);
     }
 }
---
+++ src/test/java/nl/arothuis/aoc/core/PuzzleInputTest.java
@@ -0,0 +1,21 @@
+package nl.arothuis.aoc.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PuzzleInputTest {
+    @Test
+    @DisplayName("read puzzle input by year and day under resources directory")
+    void readInputFromFile() {
+        var input = PuzzleInput.getFromFile("2023", "-test");
+        assertEquals("It works!", input);
+    }
+
+    @Test
+    @DisplayName("informs when input cannot be read from file")
+    void inputCannotBeReadFromFile() {
+        assertThrows(RuntimeException.class, () -> PuzzleInput.getFromFile("0000", "00"));
+    }
+}
