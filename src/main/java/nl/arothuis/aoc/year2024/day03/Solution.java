713f35b3a58ed37b52289a54e0a26b8af99c9232
Alex Rothuis <alex.rothuis@gmail.com>
2024-12-02 17:29:40 +0100

    Prepare 2024, day 3; add templates

---
+++ src/main/java/nl/arothuis/aoc/year2024/day03/Solution.java
@@ -0,0 +1,15 @@
+package nl.arothuis.aoc.year2024.day03;
+
+import nl.arothuis.aoc.core.PuzzleSolution;
+
+public class Solution implements PuzzleSolution<Long, Long> {
+    @Override
+    public Long solveA(String input) {
+        return -1L;
+    }
+
+    @Override
+    public Long solveB(String input) {
+        return -1L;
+    }
+}
